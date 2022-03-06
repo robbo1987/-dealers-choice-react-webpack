d63cfdbeeabc562df210817e64260823d4e920d2
Robert Albenda <ralbenda@gmail.com>
2022-03-06 18:37:44 -0500

    working on front ned

--- src/index.js
+++ src/index.js
@@ -2,4 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
-ReactDOM.render(<hr />, document.querySelector("#root"));
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      bands: [],
+      guitarists: [],
+    };
+  }
+  async componentDidMount() {
+    const bandResponse = axios.get("/api/bands");
+    const guitaristResponse = axios.get("/api/guitarists");
+    this.setState({
+      bands: bandResponse.data,
+      guitarists: guitaristResponse.data,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Robby's Guitar List</h1>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
