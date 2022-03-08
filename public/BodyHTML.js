67b60116038d80a89149f1a5692047eb7cac17ba
Robert Albenda <ralbenda@gmail.com>
2022-03-08 14:53:16 -0500

    destructured props

--- public/BodyHTML.js
+++ public/BodyHTML.js
@@ -1,29 +1,26 @@
 import React from "react";
 
-const BodyHTML = (props) => {
+const BodyHTML = ({ guitarists, bands }) => {
   return (
     <div>
       <h1>Robby's Guitar List</h1>
       <h2>Lets Start with a List of Some Cool Guitarists!</h2>
       <ul>
-        {props.guitarists.map((guitarist) => (
+        {guitarists.map((guitarist) => (
           <li key={guitarist.id}> {guitarist.name} </li>
         ))}
       </ul>
       <h2> Can YOU Guess what bands these guitarists come from??</h2>
       <h2> Lets Start With a List of the BANDS!</h2>
       <ul>
-        {props.bands.map((band) => (
-          <li key={band.id}>
-            {band.name}
-            
-          </li>
+        {bands.map((band) => (
+          <li key={band.id}>{band.name}</li>
         ))}
       </ul>
       <h2> Maybe the above List Helped?</h2>
       <h2>Here are the Answers:</h2>
       <ul>
-        {props.guitarists.map((guitarist) => (
+        {guitarists.map((guitarist) => (
           <li key={guitarist.id}>
             {guitarist.name} --- {guitarist.band.name}
           </li>
